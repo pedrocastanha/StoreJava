8b88f156162a43225bdd6526ee65bb0f895c0724
Pedro Castanheira Costa <pedrinho.castanha@gmail.com>
2025-04-15 20:13:03 -0300

    atualização db

--- scripts/init.sql
+++ scripts/init.sql
@@ -12,18 +12,18 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 -- Table: sales
-CREATE TABLE sales (
-    id UUID PRIMARY KEY,
-    user_id UUID NOT NULL,
-    payment_method TEXT NOT NULL,
-    sale_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(uuid),
+CREATE TABLE IF NOT EXISTS sales (
+    id UUID PRIMARY KEY,         -- ID único para cada venda
+    user_id UUID NOT NULL,       -- ID do usuário que fez a compra
+    payment_method TEXT NOT NULL, -- Método de pagamento (cartão, boleto, etc.)
+    sale_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data da venda
+    FOREIGN KEY (user_id) REFERENCES users(uuid)
 );
 
 -- Table: sale_products
-CREATE TABLE sale_products (
-    sale_id UUID NOT NULL,
-    product_id UUID NOT NULL,
+CREATE TABLE IF NOT EXISTS sale_products (
+    sale_id UUID NOT NULL,       -- ID da venda
+    product_id UUID NOT NULL,    -- ID do produto
     PRIMARY KEY (sale_id, product_id),
     FOREIGN KEY (sale_id) REFERENCES sales(id),
     FOREIGN KEY (product_id) REFERENCES products(uuid)
